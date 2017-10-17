56c90e9983a05f1568807011df0bf166fd157822
Nikitosh <npodguzov@yandex.ru>
2017-10-17 16:46:48 +0300

    Change Main class to utility class

--- src/main/java/com/nikitosh/spbau/Main.java
+++ src/main/java/com/nikitosh/spbau/Main.java
@@ -2,7 +2,8 @@ package com.nikitosh.spbau;
 
 import com.nikitosh.spbau.webcrawler.*;
 
-public class Main {
+public final class Main {
+    private Main() {}
 
     public static void main(String[] args) {
         WebCrawler webCrawler = new WebCrawlerImpl();
