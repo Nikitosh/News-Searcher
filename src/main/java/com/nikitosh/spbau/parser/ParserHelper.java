49d22e44c31bd3c48ed92ec330323a98ee0ec512
imbd <bd.97@mail.ru>
2017-10-18 03:32:02 +0300

    Add parser example and get link function

--- src/main/java/com/nikitosh/spbau/parser/ParserHelper.java
+++ src/main/java/com/nikitosh/spbau/parser/ParserHelper.java
@@ -1,17 +1,28 @@
 package com.nikitosh.spbau.parser;
 
-import org.jsoup.*;
-import org.jsoup.nodes.*;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
-import java.io.*;
-import java.net.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public final class ParserHelper {
     public static final String CRAWLER_BOT = "CrawlerBot";
     public static final int TIMEOUT = 2000;
+    private static final String A_HREF = "a[href]";
+    private static final String HREF = "href";
+    private static final String HTTP = "http";
     private static final String WWW = "www.";
 
-    private ParserHelper() {}
+    private ParserHelper() {
+    }
 
     public static String getDomainName(String url) throws URISyntaxException {
         URI uri = new URI(url);
@@ -26,5 +37,25 @@ public final class ParserHelper {
     public static Document getDocument(String url) throws IOException {
         Connection connection = Jsoup.connect(url).userAgent(CRAWLER_BOT).timeout(TIMEOUT);
         return connection.get();
+    }
+
+    public static List<String> getLinks(Document document, String url) {
+        List<String> res = new ArrayList<>();
+        Elements links = document.select(A_HREF);
+        for (Element el : links) {
+            String newLink = url;
+            String ending = el.attr(HREF);
+            if (ending.length() > 0 && ending.charAt(0) == '/') {
+                newLink += ending;
+            } else {
+                if ((ending.length() >= 4 && ending.substring(0, 4).equals(HTTP))) {
+                    newLink = ending;
+                } else {
+                    continue;
+                }
+            }
+            res.add(newLink);
+        }
+        return res;
     }
 }
---
+++ src/main/java/com/nikitosh/spbau/parser/ParserTASS.java
@@ -0,0 +1,76 @@
+package com.nikitosh.spbau.parser;
+
+import com.nikitosh.spbau.storage.UrlInfo;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.nikitosh.spbau.parser.ParserHelper.getDocument;
+import static com.nikitosh.spbau.parser.ParserHelper.getLinks;
+
+public class ParserTASS implements Parser {
+
+    private static final String META_NAME = "meta[name]";
+    private static final String NAME = "name";
+    private static final String PUBLISH_DATE = "publish_date";
+    private static final String CONTENT = "content";
+    private static final String DIV_CLASS = "div[class]";
+    private static final String CLASS = "class";
+    private static final String MATERIAL_TEXT = "b-material-text__l js-mediator-article";
+    private static final String ALSO_LOOK = "Смотрите также";
+
+    @Override
+    public UrlInfo parse(String url) {
+        String text = "";
+        LocalDateTime time = LocalDateTime.MIN;
+        List<String> links = new ArrayList<>();
+
+        try {
+            Document document = getDocument(url);
+            text = getText(document);
+            time = getTime(document);
+            links = getLinks(document, url);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new UrlInfo(text, time, links);
+    }
+
+    private LocalDateTime getTime(Document document) {
+        LocalDateTime time = LocalDateTime.MIN;
+        Elements info = document.select(META_NAME);
+        for (Element el : info) {
+            if (el.attr(NAME).toLowerCase().equals(PUBLISH_DATE)) {
+                String content = el.attr(CONTENT);
+                String[] dateParts = content.split(" |-|:");
+                int[] date = new int[dateParts.length];
+                for (int i = 0; i < dateParts.length; i++) {
+                    System.out.println(dateParts[i]);
+                    date[i] = Integer.valueOf(dateParts[i]);
+                }
+                time = LocalDateTime.of(date[0], date[1], date[2], date[3], date[4], date[5]);
+            }
+        }
+        return time;
+    }
+
+    private String getText(Document document) {
+        Elements elements = document.select(DIV_CLASS);
+        for (Element el : elements) {
+            if (el.attr(CLASS).toLowerCase().equals(MATERIAL_TEXT)) {
+                String result = el.text();
+                result = result.substring(0, result.indexOf(ALSO_LOOK));
+                return result;
+            }
+        }
+        return "";
+    }
+}
