6ff9033f7cb88081326d78d43f00e201fe5b48e2
Nikitosh <npodguzov@yandex.ru>
2017-11-23 00:08:14 +0300

    Add creating of dictionary with terms

--- src/main/java/com/nikitosh/spbau/dataprocessor/DataHandlerImpl.java
+++ src/main/java/com/nikitosh/spbau/dataprocessor/DataHandlerImpl.java
@@ -6,6 +6,7 @@ import org.apache.logging.log4j.Logger;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.RandomAccessFile;
 import java.util.HashMap;
 import java.util.List;
@@ -14,7 +15,8 @@ import java.util.Map;
 public class DataHandlerImpl implements DataHandler {
     private static final Logger LOGGER = LogManager.getLogger();
 
-    private static final String INDEX_PATH = "../index";
+    private static final String INDEX_PATH = "../data/data.index";
+    private static final String DICTIONARY_PATH = "../data/terms.dict";
 
     private Processor processor = new ProcessorImpl();
     private DatabaseHandler databaseHandler = new DatabaseHandler();
@@ -33,7 +35,8 @@ public class DataHandlerImpl implements DataHandler {
                 id++;
                 documentIds.put(documentFile.getName(), id);
                 List<String> terms = processor.getTermsFromFile(documentFile);
-                databaseHandler.addPageAttributes(PageAttributesExtractor.getPageAttributes(documentFile.getName(), terms));
+                databaseHandler.addPageAttributes(
+                        PageAttributesExtractor.getPageAttributes(id, documentFile.getName(), terms));
                 Map<String, DictionaryEntry> documentInvertedIndex = IndexerHelper.getInvertedIndex(id, terms);
                 for (Map.Entry<String, DictionaryEntry> entry : documentInvertedIndex.entrySet()) {
                     String term = entry.getKey();
@@ -45,7 +48,11 @@ public class DataHandlerImpl implements DataHandler {
         }
         long currentIndent = 0;
         RandomAccessFile indexFile = new RandomAccessFile(INDEX_PATH, "rw");
+        File dictionaryFile = new File(DICTIONARY_PATH);
+        PrintWriter dictionaryWriter = new PrintWriter(dictionaryFile);
         for (String term : totalEntriesSize.keySet()) {
+            dictionaryWriter.println(term);
+            dictionaryWriter.println(currentIndent);
             indexFile.seek(currentIndent);
             indexFile.writeChars(term);
             indexFile.writeInt(totalEntriesCount.get(term));
@@ -53,6 +60,7 @@ public class DataHandlerImpl implements DataHandler {
             fileIndents.put(term, currentIndent);
             currentIndent += totalEntriesSize.get(term);
         }
+        dictionaryWriter.close();
         for (File documentFile : files) {
             if (documentFile.isFile()) {
                 id = documentIds.get(documentFile.getName());
