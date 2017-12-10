4d244538a0b9484a52b0bf5035de4f8e7d4060ef
Nikitosh <npodguzov@yandex.ru>
2017-12-10 20:31:33 +0300

    Add css and js for UI

---
+++ ui/static/main.js
@@ -0,0 +1,10 @@
+(function () {
+
+  'use strict';
+
+  angular.module('NewsSearcherApp', [])
+
+  .controller('NewsSearcherController', ['$scope', '$log', '$http', '$timeout',
+    function($scope, $log, $http, $timeout) {}])
+
+}());
